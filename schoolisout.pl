a1893507365099ae402948c53387635e463236a5
Pietro Paolini <pedr0@pedr0crunchbang>
2015-10-25 19:19:19 +0000

    Shoolisout: complete, more than a solution though

--- schoolisout.pl
+++ schoolisout.pl
@@ -39,81 +39,93 @@ county(cumbria).
 
 solve :-
 	% Subject per teacher
-	subject(KnightSubject),subject(GrossSubject),subject(McEvoySubject),subject(AppletonSubject),subject(ParnellSubject),
-	all_different([KnightSubject,GrossSubject,McEvoySubject,AppletonSubject,ParnellSubject]),
+	subject(KnightSubject), subject(GrossSubject), subject(McEvoySubject), subject(AppletonSubject), subject(ParnellSubject),
+	all_different([KnightSubject, GrossSubject, McEvoySubject, AppletonSubject, ParnellSubject]),
 	
 	% Activy per teacher
-	activity(KnightActivity),activity(GrossActivity),activity(McEvoyActivity),activity(AppletonActivity),activity(ParnellActivity),
-	all_different([KnightActivity,GrossActivity,McEvoyActivity,AppletonActivity,ParnellActivity]),
+	activity(KnightActivity), activity(GrossActivity), activity(McEvoyActivity), activity(AppletonActivity), activity(ParnellActivity),
+	all_different([KnightActivity, GrossActivity, McEvoyActivity, AppletonActivity, ParnellActivity]),
 
 	% County per teacher
-	county(KnightCounty),county(GrossCounty),county(McEvoyCounty),county(AppletonCounty),county(ParnellCounty),
-	all_different([KnightCounty,GrossCounty,McEvoyCounty,AppletonCounty,ParnellCounty]),
+	county(KnightCounty), county(GrossCounty), county(McEvoyCounty), county(AppletonCounty), county(ParnellCounty),
+	all_different([KnightCounty, GrossCounty, McEvoyCounty, AppletonCounty, ParnellCounty]),
 	
 	Quadruple = [ [knight, KnightActivity, KnightCounty, KnightSubject],
 		      [gross, GrossActivity, GrossCounty, GrossSubject],
 		      [mcevoy, McEvoyActivity, McEvoyCounty, McEvoySubject],
 		      [appleton, AppletonActivity, AppletonCounty, AppletonSubject],
 		      [parnell, ParnellActivity, ParnellCounty, ParnellSubject] ],
-	
 	% First point
 	% Mr. Gross teaches either math of science
 	(	member([gross, _, _, math], Quadruple);
 		member([gross, _, _, science], Quadruple)
 	),
-	
+
 	% If Ms.Gross is going to a nudist colony then she is going
 	% to Suffolk otherwise she is going to Cornwall
 	(
 		member([gross, nudist_colony, suffolk, _], Quadruple);
-		member([gross, _, cornwall, _], Quadruple)
+		(
+			member([gross, _, cornwall, _], Quadruple),
+			\+ member([gross, nudist_colony, cornwall, _], Quadruple)
+		)
 	),
 	
+		
 	% Second point
-	% The science teacher is going to body boarding
-	member([_, body_boarding, _, science], Quadruple),
-
-	% The science teacher is going to travel to Cornwall or Norfolk
-	member([_, _, cornwall, science], Quadruple),	
-	member([_, _, norfolk, science], Quadruple),	
+	% The science teacher is going body boarding is going to travel to
+	% Cornwall or Norfolk
+	(
+		member([_, body_boarding, cornwall, science], Quadruple);	
+		member([_, body_boarding, norfolk, science], Quadruple)
+	),	
 	
 	% Third point
-	% Mr McEvoy is the history teacher
-	member([mcevoy, _, _, history], Quadruple),
-
-	% Mr. MvEvoy is going to Yorkshire or Cumbria
+	% Mr McEvoy who is the history teacher is going to Yorkshire or Cumbria
 	(
-		member([mcEeoy, _, yorkshire, _], Quadruple);
-		member([mcEeoy, _, cumbria, _], Quadruple)
-		 	
+		member([mcevoy, _, yorkshire, history], Quadruple);
+		member([mcevoy, _, cumbria, history], Quadruple)	 	
 	),
-
-	% Forth point
-	% If THE woman whis is the english teacher (triple check)
-	%KnightSubject \+ english,
-	%McEvoySubject \+ english,
 	
-	% Ms.Parnell is going swimming
-	member([parnell, _, swimming, _], Quadruple),
-
+	% Forth point
 	% If the woman who is going to Hertfordshire is the english teacher
 	% then she is Ms.Appleton otherwise she is Ms.Parnell
-	
+	% Ms.Parnell is going swimming
+
+	member([parnell, swimming, _, _], Quadruple),
+	(
+		% She teaches english therefore is Ms.Appleton	
+		member([appleton, _, hertfordshire, english], Quadruple);
+		
+		% She does not teach english, then she is Ms.Parnell
+		(
+			member([parnell, _, hertfordshire, _], Quadruple),
+			\+ member([parnell, _, _, english], Quadruple)
+		)
+	),
+
 	% Fifth point
 	% The person who is going to Yorkshire isn't the PE teacher
-	\+ member([_, yorkshire, _, pe], Quadruple),
+	\+ member([_, _, yorkshire, pe], Quadruple),
 	
 	% The person who is going to Yorkshire isn't the one who is going to
 	% sightseeing 
-	\+ member([_, yorkshire, sightseeing, _], Quadruple),
+	\+ member([_, sightseeing, yorkshire, _], Quadruple),
 	
 	% Sixth point
-	\+ member([gross, _, camping, _], Quadruple),
+	\+ member([gross, camping, _, _], Quadruple),
 	
 	% Seventh point
-	member([female(_), _, nudist_colony, _], Quadruple),	
-	
-	tell(gross, GrossSubject, GrossCounty, GrossActivity). 
+	(
+		member([gross, nudist_colony, _, _], Quadruple);
+		member([appleton, nudist_colony, _, _], Quadruple);
+		member([parnell, nudist_colony, _, _], Quadruple)
+	),
+	tell(gross, GrossSubject, GrossCounty, GrossActivity), 
+	tell(appleton, AppletonSubject, AppletonCounty, AppletonActivity), 
+	tell(parnell, ParnellSubject, ParnellCounty, ParnellActivity), 
+	tell(knight, KnightSubject, KnightCounty, KnightActivity), 
+	tell(mcevoy, McEvoySubject, McEvoyCounty, McEvoyActivity). 
 	
 
 % To be understood (!)
@@ -121,6 +133,7 @@ all_different([H | T]) :- member(H, T), !, fail.
 all_different([_ | T]) :- all_different(T).
 all_different([_]).
 
-tell(T, X, Y, Z) :- write('Professor: '), write(T), write('who teaches :'), write(X), 
-			write(' is going to: '), write(Y), write('to :'), write(Z).
+tell(T, X, Y, Z) :- write('['), write(T), write(']\t['), write(X), 
+			write(']\t['), write(Y), write(']\t['), write(Z),
+			write(']'), nl.
 	
