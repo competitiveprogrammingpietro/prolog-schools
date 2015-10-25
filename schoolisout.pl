e8f3c64dd987ebbd32c4ecb705557b6e5ed89a15
Pietro Paolini <pedr0@pedr0crunchbang>
2015-10-25 16:35:43 +0000

    schoolisout: build up the general structure of the problem's model

--- schoolisout.pl
+++ schoolisout.pl
@@ -1,17 +1,66 @@
 % Teachers
-teacher(Knight).
-teacher(Gross).
-teacher(McEvoy).
-teacher(Appleton).
-teacher(Parnell).
+teacher(knight).
+teacher(gross).
+teacher(mcEvoy).
+teacher(appleton).
+teacher(parnell).
 
 % Genders
-male(Knight).
-male(McEvoy).
+male(knight).
+male(mcEvoy).
 
 % Female
-female(Gross).
-female(Appleton).
-female(Parnell).
+female(gross).
+female(appleton).
+female(parnell).
 
-:wq
+% Subjects
+subject(math).
+subject(science).
+subject(history).
+subject(english).
+subject(pe).
+
+% Activity
+activity(nudist_colony).
+activity(body_boarding).
+activity(swimming).
+activity(sightseeing).
+activity(camping).
+
+%County
+county(cornwall).
+county(norfolk).
+county(hertfordshire).
+county(yorkshire).
+county(suffolk).
+county(cumbria).
+
+
+solve :-
+	% Subject per teacher
+	subject(KnightSubject),subject(GrossSubject),subject(McEvoySubject),subject(AppletonSubject),subject(ParnellSubject),
+	all_different([KnightSubject,GrossSubject,McEvoySubject,AppletonSubject,ParnellSubject]),
+	
+	% Activy per teacher
+	activity(KnightActivity),activity(GrossActivity),activity(McEvoyActivity),activity(AppletonActivity),activity(ParnellActivity),
+	all_different([KnightActivity,GrossActivity,McEvoyActivity,AppletonActivity,ParnellActivity]),
+
+	% County per teacher
+	county(KnightCounty),county(GrossCounty),county(McEvoyCounty),county(AppletonCounty),county(ParnellCounty),
+	all_different([KnightCounty,GrossCounty,McEvoyCounty,AppletonCounty,ParnellCounty]),
+	
+	Quadruple = [ [knight, KnightActivity, KnightCounty, KnightSubject],
+		      [gross, GrossActivity, GrossCounty, GrossSubject],
+		      [mcEvoy, McEvoyActivity, McEvoyCounty, McEvoySubject],
+		      [appleton, AppletonActivity, AppletonCounty, AppletonSubject],
+		      [parnell, ParnellActivity, ParnellCounty, ParnellSubject] ],
+	tell(X,Y,Z).
+
+% To be understood (!)
+all_different([H | T]) :- member(H, T), !, fail.
+all_different([_ | T]) :- all_different(T).
+all_different([_]).
+
+tell(X, Y, Z) :- write('Ciao').
+	
