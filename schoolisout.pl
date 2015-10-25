070e0d0c8e982f843f6961959cfade06253227a7
Pietro Paolini <pedr0@pedr0crunchbang>
2015-10-25 15:53:41 +0000

    Starting coursework

---
+++ schoolisout.pl
@@ -0,0 +1,17 @@
+% Teachers
+teacher(Knight).
+teacher(Gross).
+teacher(McEvoy).
+teacher(Appleton).
+teacher(Parnell).
+
+% Genders
+male(Knight).
+male(McEvoy).
+
+% Female
+female(Gross).
+female(Appleton).
+female(Parnell).
+
+:wq
